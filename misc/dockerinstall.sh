8aa2b83a56f7d29fb930a60d8868a0dc9195365a
Emyrk <stevenmasley@gmail.com>
2018-02-28 10:00:10 -0600

    Added a document to quickly install docker. Just helpful when moving to new machines

---
+++ misc/dockerinstall.sh
@@ -0,0 +1,14 @@
+# Install docker -- https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-16-04
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+
+sudo usermod -aG docker ${USER}
+echo "Log out and back in"
+
+# Docker-compose -- https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-16-04
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
