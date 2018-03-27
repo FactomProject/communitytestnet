6cb82bfb1795032c44c93e1169a8bb4ba5e7a9a5
Emyrk <stevenmasley@gmail.com>
2018-03-27 11:54:21 -0500

    Update start

--- factom/bin/start.sh
+++ factom/bin/start.sh
@@ -1,1 +1,1 @@
-nohup /root/bin/factomd -faulttimeout=120 -startdelay=600 -network=CUSTOM -customnet=fct_community_test -blktime=600 -debugconsole=remotehost:8093 > runlog.txt 2>&1 &
+nohup /root/bin/factomd -broadcastnum=16 -faulttimeout=120 -startdelay=600 -network=CUSTOM -customnet=fct_community_test -blktime=600 -debugconsole=remotehost:8093 > runlog.txt 2>&1 &
